e73f81c922c57d3d123d9c86b1002cb2aae9392b
Marko Horvat <bse13hideous@hotmail.com>
2017-05-20 21:37:12 +0200

    Add: 2 more API methods

--- index.php
+++ index.php
@@ -64,11 +64,67 @@ $app->post('/api/PIN', function (Request $request, Response $response) {
 //GET METHOD OTP
 //Getting OTP list if PIN is set
 
-
+$app->get('/api/OTP/{user}', function (Request $request, Response $response) {
+    $user = $request->getAttribute('user');
+	$filename = "users/".$user.".txt";
+	$userFile = fopen($filename, "rw") or die("Unable to open file!");
+	$file = fread($userFile,filesize($filename));
+	$response = $response->withJson($file);
+	$response = $response->withHeader('Content-type', 'application/json');
+	//Sending OTP list
+	return $response;
+});
 
 //AUTHENTIFICATION
 
 //POST METHOD AUTH
 //Verifying given credentials, checking OTP 
+
+$app->post('/api/auth', function (Request $request, Response $response) {
+	//Posted data
+	$data = $request->getParsedBody();
+	//Unpacking info
+	//1. PIN 2. Username
+	$info = [];
+	$info["OTP"] = $data["OTP"];
+	$info["user"] = $data["user"];
+	//Reading file
+	$filename = "users/".$info["user"].".txt";
+	$userFile = fopen($filename, "rw") or die("Unable to open file!");
+	$file = fread($userFile,filesize($filename));
+	$file = json_decode($file);
+	$OTPlist[] = $file->{"OTPlist"};
+	$FileOTP = json_decode(json_encode($OTPlist[0][$file->{"counter"}]));
+	fclose($userFile);
+	
+	//Checking if OTP and counter match
+	if ($FileOTP->{"OTP"} == $info["OTP"]){
+		//Preparing JSON for writing
+		$status = "Authentication successful";
+			
+		//Updating counter, start from 0 after 9
+		$file->{"counter"} += 1;
+		$file->{"counter"} = $file->{"counter"}%10;
+		$file = json_encode($file);
+
+		//Writing updated counter into file
+		$writeFile = fopen($filename, "w") or die("Unable to open file!");
+		fwrite($writeFile, $file);
+		fclose($writeFile);
+			
+	}else {
+		$status = "Error #003 - OTPs doesnt match, Authentication failed";
+	}
+		
+	//Response
+	$responseArray = [];
+	$responseArray["status"] = $status;
+	$responseArray["data"] = $info;
+	
+	$response = $response->withJson($responseArray);
+	$response = $response->withHeader('Content-type', 'application/json');
+	return $response;
+	
+});
 
 $app->run();
